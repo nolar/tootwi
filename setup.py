beac8a920abd6581b567d1a66c3e2c53524367b4
Sergey Vasilyev <nolar@nolar.info>
2011-07-23 17:19:05 -0700

    New version detection and storing schema. gh-8

--- setup.py
+++ setup.py
@@ -1,70 +1,25 @@
 #!/usr/bin/env python
-#
-# Versioning routines:
-# Idea from http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
-# Code from https://github.com/warner/python-ecdsa/blob/0ed702a9d4057ecf33eea969b8cf280eaccd89a1/setup.py#L34
-# Altered to support any version tagging convention and version.py location.
-#
 
 # Make sure setup.py can work with no setuptools installed initially.
-# See ez_setup.py for details (this trick is part of easyinstall).
+# See ez_setup.py for details (this trick is a part of easyinstall).
 from ez_setup import use_setuptools
 use_setuptools()
 
-import os, sys, subprocess, re
-from setuptools import setup, find_packages, Command
+# Now, our own stuff.
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+#
+# Version and overrided sdist commands:
+# Idea from http://stackoverflow.com/questions/2058802/how-can-i-get-the-version-defined-in-setup-py-setuptools-in-my-package
+# Code from https://github.com/warner/python-ecdsa/blob/0ed702a9d4057ecf33eea969b8cf280eaccd89a1/setup.py#L34
+#
+from setuptools import Command
 from setuptools.command.sdist import sdist as _sdist
 
-VERSION_PY_PATH = "tootwi/_version.py"
-VERSION_PY_TEXT = """
-# This file is generated from Git information by running 'setup.py version'.
-# Distribution tarballs contain a pre-generated copy of this file.
-
-__version__ = '%s'
-"""
-
-def update_version_py():
-    if not os.path.isdir(".git"):
-        print "This does not appear to be a Git repository."
-        return
-    try:
-        p = subprocess.Popen(["git", "describe", "--dirty", "--always"],
-                             stdout=subprocess.PIPE)
-    except EnvironmentError:
-        print "unable to run git, leaving %s alone" % (VERSION_PY_PATH)
-        return
-    stdout = p.communicate()[0]
-    if p.returncode != 0:
-        print "unable to run git, leaving %s alone" % (VERSION_PY_PATH)
-        return
-    
-    # Extract version info from git tag and put it into a file.
-    stdout = stdout.strip()
-    matches = re.match(r'^\D*?(\d+\.\d+.*?)$', stdout, re.X + re.S)
-    if not matches:
-        print "unable to find version info in tag %s, leaving %s alone" % (stdout, VERSION_PY_PATH)
-        return
-    else:
-        ver = matches.group(1)
-        f = open(VERSION_PY_PATH, "w")
-        f.write(VERSION_PY_TEXT % ver)
-        f.close()
-        print "set %s to '%s'" % (VERSION_PY_PATH, ver)
-
-def get_version():
-    try:
-        f = open(VERSION_PY_PATH)
-    except EnvironmentError:
-        return None
-    for line in f.readlines():
-        mo = re.match(r"__version__ = '([^']+)'", line)
-        if mo:
-            ver = mo.group(1)
-            return ver
-    return None
-
 class version(Command):
-    description = "update _version.py from Git repo"
+    description = "update version file from Git repo"
     user_options = []
     boolean_options = []
     def initialize_options(self):
@@ -72,15 +27,13 @@ class version(Command):
     def finalize_options(self):
         pass
     def run(self):
-        update_version_py()
-        print "Version is now", get_version()
+        print "Version is now", get_version(force_update=True)
 
 class sdist(_sdist):
     def run(self):
-        update_version_py()
         # unless we update this, the sdist command will keep using the old version
-        self.distribution.metadata.version = get_version()
-        return _sdist.run(self)
+        self.distribution.metadata.version = get_version(allow_update=True)
+        return _sdist.run(self) #NB: this is an old-style class, not supported by super()
 
 
 def read(filename):
